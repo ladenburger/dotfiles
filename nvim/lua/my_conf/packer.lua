2211efc069e2e5768381f130ef9e92e41a081d94
Benedikt Ladenburger <b.ladenburger@protonmail.com>
2024-07-20 16:26:45 +0200

    removed compiled packer

--- nvim/lua/my_conf/packer.lua
+++ nvim/lua/my_conf/packer.lua
@@ -4,45 +4,53 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-    use("wbthomason/packer.nvim")
-    use({
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        requires = { "nvim-lua/plenary.nvim" },
-    })
-    use({
-        "folke/todo-comments.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
-    })
-    use({ "rose-pine/neovim", as = "rose-pine" })
-    use({ "xiyaowong/transparent.nvim" })
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = function()
-            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-            ts_update()
-        end,
-    })
-    use("nvim-treesitter/playground")
-    use("theprimeagen/harpoon")
-    use("mbbill/undotree")
-    use("tpope/vim-fugitive")
-    use({
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        requires = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "L3MON4D3/LuaSnip" },
-        },
-    })
-    use({ "mhartington/formatter.nvim" })
-    use({
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    })
+	use("wbthomason/packer.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use({
+		"folke/todo-comments.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use({ "rose-pine/neovim", as = "rose-pine" })
+	use({ "xiyaowong/transparent.nvim" })
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
+	use("nvim-treesitter/playground")
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	})
+	use({ "mhartington/formatter.nvim" })
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
 end)
