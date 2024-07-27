48be340b7434207e24bbd0bae576eb7e3cfab7e7
Benedikt Ladenburger <b.ladenburger@protonmail.com>
2024-07-27 20:49:51 +0200

    added find in files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -13,6 +13,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-vim.keymap.set("n", "<leader>pl", "<cmd>Telescope live_grep<CR>")
+     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>pg', builtin.current_buffer_fuzzy_find, {})
