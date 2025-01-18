4f3170a64f446edf403d6ad717241d0831195ca6
ladenburger <b.ladenburger@protonmail.com>
2025-01-18 21:56:24 +0100

    add background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -10,16 +10,6 @@ return {
 		vim.cmd("let g:gruvbox_material_enable_bold=1")
 		vim.cmd("let g:gruvbox_material_enable_italic=1")
 		vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
-		-- changing bg and border colors
-		vim.api.nvim_set_hl(0.7, "FloatBorder", { link = "Normal" })
-		--vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
-		vim.api.nvim_set_hl(0.7, "NormalFloat", { link = "Normal" })
-		vim.api.nvim_set_hl(0.7, "Pmenu", { link = "Normal" })
-		vim.api.nvim_set_hl(0.7, "PmenuSel", { link = "Search" })
-		vim.api.nvim_set_hl(0.7, "BlinkCmpMenu", { link = "Normal" })
-		vim.api.nvim_set_hl(0.7, "BlinkCmpMenuBorder", { link = "Normal" })
-		vim.api.nvim_set_hl(0.7, "BlinkCmpMenuSelection", { link = "Search" })
-		vim.api.nvim_set_hl(0.7, "BlinkCmpLabelMatch", { link = "Search" })
 	end,
 }
 
