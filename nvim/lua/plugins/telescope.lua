aa565b8025b4e6ed0999daa2cdfdf0d2ba6766be
ladenburger <b.ladenburger@protonmail.com>
2025-02-09 17:34:07 +0100

    add search for references

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', 'gR', builtin.lsp_references, {})
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
