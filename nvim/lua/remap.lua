e85740fc14deec4df1632e035ab342790e47ef87
ladenburger <b.ladenburger@protonmail.com>
2025-03-31 20:07:43 +0200

    reformat

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,21 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
 vim.keymap.set("x", "<leader>p", '"_dp')
-
 vim.api.nvim_set_keymap("n", "<leader>i", ":lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
-
-
-
 vim.keymap.set('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
 vim.keymap.set('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
