21ddc8550f5e06b1ac15ca7f64282fe5af7143ff
ladenburger <b.ladenburger@protonmail.com>
2025-03-31 20:06:39 +0200

    reformat

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,9 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    --  dependencies = { 'nvim-lua/plenary.nvim' }
     config = function()
         require('telescope').setup{
             defaults = {
-                file_ignore_patterns = { 
+                file_ignore_patterns = {
                     "node_modules",
                     "target",
                     "dist",
