0a5ef2bd1c815fd2d626db69905d11e56394ec30
ladenburger <b.ladenburger@protonmail.com>
2025-11-26 10:46:32 +0100

    add insert for timestamps and dates

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -9,23 +9,61 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", '"_dp')
 vim.api.nvim_set_keymap("n", "<leader>i", ":lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
-vim.keymap.set('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
-vim.keymap.set('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
-vim.keymap.set('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
-vim.keymap.set('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-vim.keymap.set('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-vim.keymap.set('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
-vim.keymap.set('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
-vim.keymap.set('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-vim.keymap.set('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
-vim.keymap.set('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+vim.keymap.set("n", "<leader>gw", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+vim.keymap.set("n", "<leader>gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
+vim.keymap.set("n", "<leader>ah", "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n", "<leader>af", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+vim.keymap.set("n", "<leader>ee", "<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>")
+vim.keymap.set("n", "<leader>ar", "<cmd>lua vim.lsp.buf.rename()<CR>")
+vim.keymap.set("n", "<leader>=", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+vim.keymap.set("n", "<leader>ai", "<cmd>lua vim.lsp.buf.incoming_calls()<CR>")
+vim.keymap.set("n", "<leader>ao", "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>")
 vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>Y", '"+yy', { noremap = true, silent = true })
 
+-------------------------------------------------------------------
+-- Insert helpers (dates / timestamps)
+-------------------------------------------------------------------
+local Insert = {}
+
+function Insert.put(text, no_space)
+	local pos = vim.api.nvim_win_get_cursor(0)
+	local row = pos[1]
+	local col = pos[2]
+
+	local prefix = no_space and "" or " "
+	vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { prefix .. text })
+end
+
+function Insert.date(no_space)
+	Insert.put(os.date("%Y-%m-%d"), no_space)
+end
+
+function Insert.timestamp(no_space)
+	Insert.put(os.date("%Y-%m-%d %H:%M"), no_space)
+end
+
+-- Keymaps for inserting date/time
+vim.keymap.set("n", "<leader>td", function()
+	Insert.date(false)
+end, { desc = "Insert date (with space)" })
+
+vim.keymap.set("n", "<leader>ts", function()
+	Insert.timestamp(false)
+end, { desc = "Insert timestamp (with space)" })
+
+vim.keymap.set("n", "<leader>tD", function()
+	Insert.date(true)
+end, { desc = "Insert date (no space)" })
+
+vim.keymap.set("n", "<leader>tS", function()
+	Insert.timestamp(true)
+end, { desc = "Insert timestamp (no space)" })
