d6d2bdb0089a9407111e7cc7ae388bbc2f17fd54
ladenburger <b.ladenburger@pm.me>
2026-03-29 19:34:06 +0200

    update treesitter to new config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,21 +2,14 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "javascript", "typescript", "lua", "rust" },
+		require("nvim-treesitter").setup({
+			ensure_installed = {
+				"c", "lua", "vim", "vimdoc", "query",
+				"elixir", "heex", "javascript", "typescript",
+				"html", "rust",
+			},
 			sync_install = false,
 			auto_install = true,
-
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
